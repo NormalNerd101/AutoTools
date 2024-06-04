0d7c50e4aa7e0e154dba1166b2d3d49ce16c3331
VincentCody <nhuyhoang1012005@gmail.com>
2024-06-04 10:49:57 +0700

    Quick Update (06/04/24)

--- collected_mini_projects/Addfriendbot.py
+++ collected_mini_projects/Addfriendbot.py
@@ -1,20 +1,24 @@
+#!/usr/bin/env python
+import os
 import time
 
 from selenium import webdriver  # selenium setup
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-PROCESSOR = '\033[95m'         # Light pink
-BLUE = '\033[94m'              # Blue
-GREEN = '\033[92m'             # Green
-YELLOW = '\033[93m'            # Yellow
-FAIL = '\033[91m'              # Red
+os.system("")
+PROCESSOR = '\033[95m'  # Light pink
+BLUE = '\033[94m'  # Blue
+GREEN = '\033[92m'  # Green
+YELLOW = '\033[93m'  # Yellow
+FAIL = '\033[91m'  # Red
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
 driver = webdriver.Firefox()
+
 
 def help():  # help function: instruction.
     print(BLUE + "start : Start the script." + ENDC)
@@ -27,83 +31,86 @@ def help():  # help function: instruction.
 
 tool = "Add_friends@py: "
 cycle = True  # login cycle
-
-while (True):  # the script
-    c = input(PROCESSOR + tool + ENDC)  # command-line-like interface
-    if (c == "start"):                # start script
-        x = int(input(BOLD + "Number of friends to add:\t" + ENDC))  # number of friends to add
-        added = x                                                    # number of soon-will-be-added accounts. save up for modifying
-
-        while cycle:  # cycle while the script cannot login
-
-            nme = input(YELLOW + "Your facebook username/email: " + ENDC)  # facebook email / username
-            pssword = input(YELLOW + "Your facebook password: " + ENDC)    # facebook password
-
-            driver.get("https://www.facebook.com")  # open to facebook.com
-
-            email = driver.find_element(By.NAME, "email")  # search email textbox
-            email.send_keys(nme)                           # enter email / username = nme
-
-            # email.send_keys(Keys.TAB)  # probably useless, since the next step covers the intent already.
-
-            password = driver.find_element(By.NAME, "pass")  # search password textbox
-
-            password.send_keys(pssword)      # enter password
-            password.send_keys(Keys.RETURN)  # --press login button.
-
-            breakpoint()    # ain't no idea but we gotta wait for a while till the process is set.
-
-            try:                                                                               # try finding home-button and click, test for login success.
-                driver.find_element(By.CSS_SELECTOR, '[aria-label="Home"]').click()  # find Home-button and click it
-                cycle = False;                                                                 # if succeded, not cycle
-                break;                                                                         # break out of the cycle
-            except NoSuchElementException:                                                     # if password and/or username are incorrect
-                print(FAIL + "Your username and/or password appear to be incorrect. Try again !" + ENDC)    # report error
+times = 0
+# the script
+while True:
+    print("--Commands : start - version - exit - info - clear - help.")
+    c = input(PROCESSOR + tool + ENDC)
+    if c == "start":
+        x = int(input(BOLD + "Number of friends to add: " + ENDC))
+        added = x
+        # log-in cycle.
+        while cycle:
+            # open facebook.com
+            driver.get("https://www.facebook.com")
+            # Enter account and password
+            nme = input(YELLOW + "Your facebook username/email: " + ENDC)
+            pssword = input(YELLOW + "Your facebook password: " + ENDC)
+            email = driver.find_element(By.NAME, "email")
+            email.send_keys(nme)
+            password = driver.find_element(By.NAME, "pass")
+            password.send_keys(pssword)
+            password.send_keys(Keys.RETURN)
+            times += 1
+            time.sleep(8)
+            # Test for log-in success.
+            # breakpoint()
+            if times == 5:
+                print(FAIL + "OVERLOADING ERR : TERMINATING . . . " + ENDC)
+                exit(1)
+            try:
+                # WebDriverWait(driver, 12).until(
+                #     EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Block')]"))
+                # ).click()
+                driver.refresh()
+                time.sleep(4)
+                driver.find_element(By.CSS_SELECTOR, '[aria-label="Home"]').click()
+                cycle = False
+                break
+            except:
+                print(
+                    FAIL + "Your username and/or password appear to be incorrect. Or Errors occured while logging-in. "
+                           "Try again !" + ENDC)
                 cycle = True
 
-        time.sleep(5)  # wait for process loading ( redundant ?)
         print(PROCESSOR + "Successfully Logged in. continuing !" + ENDC)
 
-        for friend_index in range(1, x + 1):                                            # friend-adding cycle
-            if friend_index == 1 :
-                driver.find_element(By.CSS_SELECTOR, '[aria-label="Friends"]').click()         # click the Friends button
-                time.sleep(3)                                                                  # wait to load
-            try:     # try to click Add Friend-button
-                driver.find_element(By.CSS_SELECTOR, '[aria-label="Add friend"]').click()    # -- testing --> success. Still in checking.
-                # driver.get("https://www.facebook.com")                                     # reloads : facebook will not think that the script is a bot + time delay (new friends may appear) --prbly won't be of use.
-
-                print(UNDERLINE + "Friend number " + GREEN + str(friend_index) + ENDC + UNDERLINE + " has been added." + ENDC)  # reports
-                time.sleep(2)       # wait for loading process
-
-            except NoSuchElementException:                                # if button not found
-                time.sleep(3)                                             # wait (in hope that new Add Friend-buttons will appear)
-                print(FAIL + "Error, friend couldn't been added" + ENDC)  # report
-                added = added - 1                                         # loses one friend
-                # driver.get('https://www.facebook.com')                    # reload to check if new friends appeared (a bit redundant. prbly won't be of use).
-
-        print(YELLOW + "You have succesfully added " + str(added) + " friends in total!. \n" + ENDC)    # reports how many friends were succesfully added
-        print(PROCESSOR + "Terminating . . . ")
-        driver.close()                                         # clothes firefox window
+        # friend-adding process.
+        for friend_index in range(1, x + 1):
+            if friend_index == 1:
+                driver.find_element(By.CSS_SELECTOR, '[aria-label="Friends"]').click()
+                time.sleep(3)
+            # try to click Add Friend-button
+            try:
+                driver.find_element(By.CSS_SELECTOR, '[aria-label="Add friend"]').click()
+                # driver.get("https://www.facebook.com")    # reloads : facebook will not think that the script is a
+                # bot + time delay (new friends may appear)
+                print(UNDERLINE + "Friend number " + GREEN + str(
+                    friend_index) + ENDC + UNDERLINE + " has been added." + ENDC)
+                time.sleep(2)
+            except NoSuchElementException:
+                time.sleep(3)  # wait (in hope that new Add Friend-buttons will appear)
+                print(FAIL + "Error, friend couldn't been added" + ENDC)
+                added = added - 1
+                # driver.get("https://www.facebook.com")    # reloads
+        # report final result.
+        print(YELLOW + "You have succesfully added " + str(added) + " friends in total!. \n" + ENDC)
+        print(PROCESSOR + "Terminating . . . " + ENDC)
+        driver.close()
         quit()
 
-    # COMMAND-LINE for interaction
-    elif (c == "exit"):             # if user wants to exit
-        print(FAIL + "Exiting...")  # report
-        driver.close()              # close firefox window
-        quit()                      # exit this script
-
-    elif (c == "version"):          # if user want to know the version number
-        print('Version 1.1 \n')     # version
-
-    elif (c == "info"):             # if user want info
-        print(
-            """This script is a simple facebook friend-request sender, it is still in development. If you wish to help or give an advice or an idea, you are welcome at """ + BLUE + UNDERLINE + """https://gist.github.com/RRobotek/6619b5ca6948f1f49ae3\n""" + ENDC + """Cheers, RRobotek \n""")
-
-    elif (c == "clear"):  # clear
-        print('\n' * 64)  # 64 new lines
-
-    elif (c == "help"):   # help
-        help()            # function above is used
-
-    else:                 # if command not recognized
+    # COMMAND-LINEs
+    elif c == "exit":
+        print(FAIL + "Exiting...")
+        driver.close()
+        quit()
+    elif c == "version":
+        print('Version 1.5 \n')
+    elif c == "info":
+        print(BLUE + "A Facebook friend-request sender script for your entertainment. Have fun ! " + ENDC)
+    elif c == "clear":
+        print('\n' * 64)
+    elif c == "help":
+        help()
+    else:
         print(FAIL + "Command not recognized. Please try again !" + ENDC)
---
+++ collected_mini_projects/unfriendbot.py
@@ -0,0 +1,102 @@
+import os
+import logging
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+os.system("")
+PROCESSOR = '\033[95m'  # Light pink
+BLUE = '\033[94m'  # Blue
+GREEN = '\033[92m'  # Green
+YELLOW = '\033[93m'  # Yellow
+FAIL = '\033[91m'  # Red
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Set up WebDriver options
+options = Options()
+options.headless = False
+driver = webdriver.Chrome(options=options)
+
+# Account info
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+max_attempts = 5
+attempts = 0
+target = "https://www.facebook.com"
+
+def login_to_facebook(driver, email, password):
+    while driver.current_url != target:
+        driver.get("https://www.facebook.com")
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email"))).send_keys(email)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "pass"))).send_keys(password + Keys.RETURN)
+        time.sleep(6)
+
+def navigate_to_friends_section(driver):
+    try:
+        profile_link = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'a[href="https://www.facebook.com/profile.php?id=100014596003638"]'))
+        )
+        profile_link.click()
+
+        friends_link = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'a[href="https://www.facebook.com/profile.php?id=100014596003638&sk=friends"]'))
+        )
+        friends_link.click()
+        print("Successfully entered Friends section.")
+    except Exception as e:
+        logging.error(f"Error navigating to friends section: {e}")
+        return False
+    return True
+
+def unfriend_perform(driver):
+    try:
+        friends_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Friends"][role="button"]'))
+        )
+        friends_button.click()
+
+        menu_item = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '[role="menuitem"][tabindex="-1"]'))
+        )
+        menu_item.click()
+
+        confirm_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Confirm"]'))
+        )
+        confirm_button.click()
+        time.sleep(4)
+    except Exception as e:
+        logging.error(f"Error performing friend actions: {e}")
+        return False
+    return True
+
+
+while attempts < max_attempts:
+    try:
+        login_to_facebook(driver, email, password)
+        print("Successfully logged in :)")
+        time.sleep(3)
+        driver.refresh()
+        if navigate_to_friends_section(driver):
+            while unfriend_perform(driver):
+                pass
+            logging.info("Successfully unfriended all in friend actions.")
+            break
+    except Exception as e:
+        logging.error(f"Login attempt {attempts + 1} failed: {e}")
+        attempts += 1
+        if attempts >= max_attempts:
+            logging.error("OVERLOADING ERR : TERMINATING . . . ")
+            break
